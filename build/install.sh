0bb28376c74ead71fd0f6361e0b835d3d50d1217
Tom Day <Tom.Day@bjss.com>
2019-09-05 10:38:09 +0100

    Realign Submodules, and insallers

--- build/install.sh
+++ build/install.sh
@@ -1,10 +1,3 @@
-echo "Running Dependency Installation"
-mkdir -p lib
-cd "lib"
-rm -rf "Library.Ecommerce.Framework"
-git clone https://github.com/PseudonymLtd/Library.Ecommerce.Framework.git
-cd "Library.Ecommerce.Framework"
-npm run clean
-npm install
-cd "../../"
-npm link lib/Library.Ecommerce.Framework
+echo "Installing Visual Studio Code Support"
+mkdir -v -p "../.vscode"
+cp -v "build/vscode-settings.json" "../.vscode/launch.json"
